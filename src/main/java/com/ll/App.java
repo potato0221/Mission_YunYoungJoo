62c25587578dbb93ad7aace7280ef7a8d8aa92f5
potato0221 <wnqo10@naver.com>
2023-11-06 10:23:30 +0900

    등록,명언 번호 증가

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,15 +1,19 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
     private Scanner scanner;
-
+    private int idx;
+    private List<Saying> sayings;
 
     App() {
         scanner = new Scanner(System.in);
-
+        idx=0;
+        sayings=new ArrayList<>();
     }
 
     public void run() {
@@ -20,7 +24,22 @@ public class App {
             if (cmd.equals("종료")) {
                 break;
 
+            }else if(cmd.equals("등록")){
+                addSaying();
+
             }
         }
     }
+
+    private void addSaying(){
+        System.out.print("명언 : ");
+        String content=scanner.nextLine();
+        System.out.print("작가 : ");
+        String author=scanner.nextLine();
+        idx++;
+        Saying saying=new Saying(idx,content,author);
+        sayings.add(saying);
+        System.out.printf("%d번 명언이 등록 되었습니다.\n",idx);
+    }
+
 }
---
+++ src/main/java/com/ll/Saying.java
@@ -0,0 +1,13 @@
+package com.ll;
+
+public class Saying {
+    public int idx;
+    public String content;
+    public String author;
+
+    public Saying(int idx,String content, String author){
+        this.idx=idx;
+        this.content=content;
+        this.author=author;
+    }
+}
