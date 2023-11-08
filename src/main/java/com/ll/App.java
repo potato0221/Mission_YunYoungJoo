87c8e60e7d0e4a865332c2c05e95e74e0406231b
potato0221 <wnqo10@naver.com>
2023-11-08 11:52:05 +0900

    리팩토링

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,34 +1,24 @@
 package com.ll;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class App {
-
     private Scanner scanner;
-    private int idx;
-    private List<Saying> sayings;
+
 
     App() {
         scanner = new Scanner(System.in);
-        idx = 0;
-        sayings = new ArrayList<>();
+
     }
 
     public void run() {
+        SayingControl sayingControl=new SayingControl(scanner);
+
+
         System.out.println("== 명언 앱 ==");
         //loadFile("sayings.txt");
-        loadJson("data.json");
-        if (!sayings.isEmpty()) {
-            idx = sayings.get(sayings.size() - 1).idx;
-        }
+        sayingControl.loadJson("data.json");
+
         while (true) {
 
             System.out.print("명령) ");
@@ -37,20 +27,20 @@ public class App {
             switch (rq.getAction()) {
                 case "종료":
                     //makeFile(sayings, "sayings.txt");
-                    makeJson(sayings, "data.json");
+                    sayingControl.makeJson("data.json");
                     return;
 
                 case "등록":
-                    addSaying();
+                    sayingControl.addSaying();
                     break;
                 case "목록":
-                    sayingList();
+                    sayingControl.sayingList();
                     break;
                 case "삭제":
-                    deleteSaying(rq);
+                    sayingControl.deleteSaying(rq);
                     break;
                 case "수정":
-                    modifySaying(rq);
+                    sayingControl.modifySaying(rq);
                     break;
                 default:
                     System.out.println("다시 입력 해 주세요.");
@@ -60,165 +50,7 @@ public class App {
     }
 
 
-    private void addSaying() {
-        System.out.print("명언 : ");
-        String content = scanner.nextLine();
-        System.out.print("작가 : ");
-        String author = scanner.nextLine();
-        idx++;
-        Saying saying = new Saying(idx, content, author);
-        sayings.add(saying);
-        System.out.printf("%d번 명언이 등록 되었습니다.\n", idx);
-    }
 
-    private void sayingList() {
-        System.out.println("번호 / 작가 / 명언");
-        System.out.println("-------------------");
-        for (int i = sayings.size() - 1; i >= 0; i--) {
-            Saying saying = sayings.get(i);
-            System.out.printf("%d / %s / %s\n", saying.idx, saying.author, saying.content);
-        }
-
-    }
-
-    private void deleteSaying(Rq rq) {
-        int id = rq.getParamAsInt("id", 0);
-        if (id == 0) {
-            System.out.println("id를 정확히 입력 해 주세요.");
-            return;
-        }
-        int deleteIdx = getIdx(id);
-        if (deleteIdx == -1) {
-            System.out.printf("%d번 명언은 존재 하지 않습니다.\n", id);
-            return;
-        }
-        sayings.remove(deleteIdx);
-        System.out.printf("%d번 명언을 삭제 했습니다.\n", id);
-
-
-    }
-
-    private void modifySaying(Rq rq) {
-        int id = rq.getParamAsInt("id", 0);
-        if (id == 0) {
-            System.out.println("id를 정확히 입력 해 주세요.");
-            return;
-        }
-        int modifyIdx = getIdx(id);
-        if (modifyIdx == -1) {
-            System.out.printf("%d번 명언은 존재 하지 않습니다.\n", id);
-            return;
-        }
-        Saying saying = sayings.get(modifyIdx);
-        System.out.printf("명언(기존) : %s\n", saying.content);
-        System.out.print("명언 : ");
-        saying.content = scanner.nextLine();
-        System.out.printf("작가(기존) : %s\n", saying.author);
-        System.out.print("작가 : ");
-        saying.author = scanner.nextLine();
-
-    }
-
-    private int getIdx(int id) {
-        for (int i = 0; i < sayings.size(); i++) {
-            Saying saying = sayings.get(i);
-            if (saying.idx == id) {
-                return i;
-            }
-        }
-
-        return -1;
-    }
-
-//    private void makeFile(List<Saying> sayings, String filename) {
-//        List<Saying> exist = loadFile("sayings.txt");
-//
-//        try (PrintWriter writer = new PrintWriter(new FileWriter("sayings.txt"))) {
-//            if (!exist.isEmpty()) {
-//                for (int i = 0; i < exist.size(); i++) {
-//                    writer.println(exist.get(i));
-//                }
-//            }
-//            for (Saying saying : sayings) {
-//                String txtSaying = saying.toString();
-//                boolean trueFalse = false;
-//                for (Saying saying1 : exist) {
-//                    if (txtSaying.contains(saying1.toString())) {
-//                        trueFalse = true;
-//                        break;
-//                    }
-//                }
-//                if (!trueFalse) {
-//                    writer.println(txtSaying);
-//                }
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-//
-//    private List<Saying> loadFile(String filename) {
-//        List<Saying> file = new ArrayList<>();
-//        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
-//            String line;
-//            while ((line = reader.readLine()) != null) {
-//                String[] parts = line.split(",");
-//                if (parts.length == 3) {
-//                    int idx = Integer.parseInt(parts[0]);
-//                    String author = parts[1];
-//                    String content = parts[2];
-//                    Saying saying = new Saying(idx, content, author);
-//                    file.add(saying);
-//                    sayings.add(saying);
-//                }
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return file;
-//
-//    }
-
-    private void makeJson(List<Saying> sayings, String fileName) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-
-        try {
-            File file = new File(fileName);
-            List<Saying> existSayings = loadJson(fileName);
-            for (Saying saying : sayings) {
-                boolean trueFalse = existSayings.stream()
-                        .anyMatch(existingSaying -> existingSaying.idx == saying.idx);
-                if (!trueFalse) {
-                    existSayings.add(saying);
-                }
-            }
-            objectMapper.writeValue(file, existSayings);
-        } catch (
-                IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private List<Saying> loadJson(String fileName) {
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        try {
-            File file = new File(fileName);
-
-            if (file.exists()) {
-                sayings = objectMapper.readValue(file, new TypeReference<List<Saying>>() {
-                });
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-
-        }
-
-        return sayings;
-    }
 
 
 }
